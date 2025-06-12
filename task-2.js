42f3b5d1b2e019147c7d5cd063675130b3434c37
Yasin Özmen <yasinozmeen3@gmail.com>
2025-06-12 10:27:44 +0300

    task2

--- task-2.js
+++ task-2.js
@@ -1,1 +1,34 @@
+function calcAverageCalories(days) {
+  let day = 0;
+  let totalCal = 0;
+  for (const i of days) {
+    day += 1;
+    totalCal += i.calories;
+  }
+  return (day = 0 ? 0 : totalCal / day);
+}
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 },
+  ])
+); // 3180
 
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 },
+  ])
+); // 2270
+
+console.log(calcAverageCalories([])); // 0
