d1983d49981fc5c9a8764de444e9bdd90841b59c
Yasin Özmen <yasinozmeen3@gmail.com>
2025-06-12 10:35:09 +0300

    task 3 finito

--- task-3.js
+++ task-3.js
@@ -1,1 +1,20 @@
+let profile = {
+  username: "Jacob",
+  playTime: 300,
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+  changeUsername(name) {
+    this.username = name;
+  },
+  updatePlayTime(time) {
+    this.playTime += time;
+  },
+};
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
